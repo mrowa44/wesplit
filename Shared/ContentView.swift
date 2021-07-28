8f02af1a2ed10b62eecdcc002216a05cb756df55
Justyna Rachowicz <mrowa44@gmail.com>
2021-07-28 18:32:16 +0200

    Wow what a crappy code

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -13,6 +13,17 @@ struct ContentView: View {
     @State private var tipPercentage = 2
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: String {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        let roundedPerPerson = String(format: "%.2f", amountPerPerson)
+        return roundedPerPerson
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -35,8 +46,8 @@ struct ContentView: View {
                     }.pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section {
-                    Text("$\(checkAmount)")
+                Section(header: Text("Total per person")) {
+                    Text("$\(totalPerPerson)")
                 }
             }
         }
