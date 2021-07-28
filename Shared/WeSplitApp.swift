afcab6bea516fd7bfdd98621cebfb22dc6bb2d94
Justyna Rachowicz <mrowa44@gmail.com>
2021-07-28 17:30:32 +0200

    Initial Commit

---
+++ Shared/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  Shared
+//
+//  Created by Justyna Rachowicz on 28/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
